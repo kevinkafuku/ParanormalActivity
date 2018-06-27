63169ecdb830e068f3c4ba2887cb28fa5d1e4458
Lori Argentum <lori.smith@richmond.edu>
2018-06-26 20:04:05 -0400

    search bar now works and is able to pull articles by name/location or both. Zip codes dont work because ppl dont reference zip codes in articles haha

--- assets/javascript/newsAPI.js
+++ assets/javascript/newsAPI.js
@@ -1,36 +1,7 @@
 var keywords = ["aliens", "paranormal", "ghosts", "bigfoot"];
-var queryURL = "https://newsapi.org/v2/top-headlines?country=au&category=" + "science" + "&apiKey=c3c56561b15f423f8748a3b782996fd3";   
-
-
-    //When the AJAX request is copmplete,
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response) {
-            console.log(response);
-    
-            
-               // <li class="list-group-item"><a href>Cras justo odio</li>
-               
 
 
 
-            var results = response.articles;
-            for (var i = 0; i < results.length; i++) {
-                var listItem = $("<a href></a>", {class:'list-group-item'});
-                listItem.text(results[i].title);
-                $(listItem).attr("href", results[i].url);
-                $(".list-group").append(listItem);
-                console.log(listItem.href);
-
-
-                
-
-             
-              }
-              
-    
-        });
 
 function buttonReview() {
     for (var i = 0; i < keywords.length; i++) {
@@ -43,15 +14,49 @@ function buttonReview() {
     }
   }
 
+
+
 $(document).ready(function() { 
     buttonReview();
 
-    
+    $("#search").on("click", function() {
+    event.preventDefault();
+    console.log("search Button clicked");
 
+    var userInput = $("#location-name-input").val().trim();
+    var queryURL = "https://newsapi.org/v2/everything?q=" + userInput + "&apiKey=c3c56561b15f423f8748a3b782996fd3";   
+        console.log(userInput);
+        console.log(queryURL);
 
-    $("body").on("click", "button", function() { 
+    //When the AJAX request is copmplete,
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            // <li class="list-group-item"><a href>Cras justo odio</li>
                
-    console.log("searching something");
+
+
+            //displays articles in the list
+            var results = response.articles;
+            for (var i = 0; i < results.length; i++) {
+                var listItem = $("<a href></a>", {class:'list-group-item'});
+                listItem.text(results[i].title);
+                $(listItem).attr("href", results[i].url);
+                $(".list-group").append(listItem);
+                console.log(listItem.href);
+
+
+                
+
+             
+            }
+    });
+    $("#location-name-input").val(""); //Clears the keyword input field
+    $("#article-display").html(""); //Clears the article display
+
+               
 
     
 });
